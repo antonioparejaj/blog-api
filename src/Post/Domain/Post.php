a91f1491bbc794a42dd99daabe20fa147b4448e2
Antonio Pareja <antonioparejaj@gmail.com>
2022-09-23 13:45:18 +0200

    create entities: post and author

---
+++ src/Post/Domain/Post.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Post\Domain;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $authorId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $tittle = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $body = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthorId(): ?int
+    {
+        return $this->authorId;
+    }
+
+    public function setAuthorId(int $authorId): self
+    {
+        $this->authorId = $authorId;
+
+        return $this;
+    }
+
+    public function getTittle(): ?string
+    {
+        return $this->tittle;
+    }
+
+    public function setTittle(string $tittle): self
+    {
+        $this->tittle = $tittle;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+}
